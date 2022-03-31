b74757c50cfd770c1e836a2153036ed5c924deb4
Sebastian Zuzunaga <211006957@aluno.unb.br>
2022-03-31 11:51:09 -0300

    Terminada a página de dicas

---
+++ src_sz/js_sz/infocovid.js
@@ -0,0 +1,19 @@
+function MostrarOcultar( ref ) 
+{  
+  // Nodo del documento que se [muestra /oculta]
+	// Tiene que tener un ID unico
+  var nodoContraible  = document.getElementById(ref); 
+	
+	// Nodo que contiene el [enlace] que maneja el que se [muestra /oculta]
+	// DEBE tener el mismo ID que el nodo [nodoContraible] pero precedido por una x
+  var xNodoDelEnlace = document.getElementById("x" + ref); 
+  
+  if (nodoContraible.style.display =='none') 
+  { 
+	   nodoContraible.style.display ='block'; 
+  } 
+  else 
+  { 
+    nodoContraible.style.display ='none'; 
+  } 
+} 
