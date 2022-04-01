39ae2a3b0cba9a0925c580552897d93652f3d284
leo <211031762@aluno.unb.br>
2022-04-01 19:03:37 -0300

    adição de funcionalidades

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -1,7 +1,44 @@
 import { openDb } from "../configDB.js";
 
+export async function checksExistsUserAccount(request, response, next) {
+  const db = await openDb();
+  const { email } = request.body;
+  const sql = `SELECT * FROM user WHERE email = ?`;
+  const user = await db.get(sql, [email]);
+  if (user) {
+    response.status(400).json({ error: "User already exists" });
+  } else {
+    next();
+  }
+}
+
+export async function checksDontExistUserAccount(request, response, next) {
+  const db = await openDb();
+  const { email } = request.body;
+  const sql = `SELECT * FROM user WHERE email = ?`;
+  const user = await db.get(sql, [email]);
+  if (!user) {
+    response.status(400).json({ error: "User doesn't exists" });
+  } else {
+    next();
+  }
+}
+
+export async function checkPassword(request, response, next) {
+  const db = await openDb();
+  const { email, password } = request.body;
+  const sql = `SELECT * FROM user WHERE email = ?`;
+  const user = await db.get(sql, [email]);
+  if (user.senha !== password) {
+    return response.status(400).json({ error: "Email or password is wrong" });
+  } else {
+    request.user = user;
+    next();
+  }
+}
+
 export async function createTableUser() {
-  openDb().then(db => { db.exec('CREATE TABLE IF NOT EXISTS user ( id INTEGER PRIMARY KEY,carinbo_data_hora DATE NOT NULL, curso VARCHAR(255) NOT NULL,nome VARCHAR(255) NOT NULL,sobrenome VARCHAR(255) NOT NULL, matricula VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, campus VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL)') })
+  openDb().then(db => { db.exec('CREATE TABLE IF NOT EXISTS user ( id INTEGER PRIMARY KEY,carinbo_data_hora DATE NOT NULL, curso VARCHAR(255) NOT NULL,nome VARCHAR(255) NOT NULL,sobrenome VARCHAR(255) NOT NULL, matricula VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, campus VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL )') })
 }
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 // import { openDb } from './configDB.js';
 import { createTablePlot, InsertPlot } from './controller/plot.js'
-import { createTableUser, InsertUser } from './controller/user.js'
+import { checkPassword, checksDontExistUserAccount, checksExistsUserAccount, createTableUser, InsertUser } from './controller/user.js'
 import express from 'express';
 
 const app = express()
@@ -12,14 +12,16 @@ createTablePlot()
 createTableUser()
 
 
-
-app.post('/cadastro', (req, res) => {
+app.post('/cadastro', checksExistsUserAccount, (req, res) => {
   InsertUser(req.body);
-  console.log(req.body)
-  res.json({ "statusCode": 200 })
+  res.status(200).json({ message: "Usuário cadastrado com sucesso" })
 });
 
+app.get('/login', checksDontExistUserAccount, checkPassword, (req, res) => {
+  const { user } = req;
+  res.status(200).json({ message: "Usuário logado com sucesso", user })
 
+})
 
 app.post('/plot', (req, res) => {
   InsertPlot(req.body);
