e5e5a9564dc6a859c56837b127a6da42a6a40b23
leo <211031762@aluno.unb.br>
2022-04-04 19:23:03 -0300

    correção do metodo de login

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,11 @@ import { getData, checCad } from './controller/plot.js'
 import express from 'express';
 import cors from 'cors';
 
+//iniciando o express
 const app = express()
 
+
+// configuração do express
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
@@ -17,10 +20,12 @@ app.use((req, res, next) => {
 app.use(express.json())
 const port = 3000
 
+//crianção das tabelas no banco de dados
 createTablePlot()
 createTableUser()
 
-
+//cadastro do usuario
+// a funçao checksExistsUserAccount verifica se o usuario ja existe no banco de dados
 app.post('/cadastro', checksExistsUserAccount, (req, res) => {
 
   const data = req.body
@@ -33,24 +38,34 @@ app.post('/cadastro', checksExistsUserAccount, (req, res) => {
   }
 });
 
-app.get('/login', checksDontExistUserAccount, checkPassword, (req, res) => {
+//login do usuario
+//a função cheksDontExistUserAccount verifica se o usuario não existe no banco de dados
+//a função checkPassword verifica se a senha está correta
+app.post('/login', checksDontExistUserAccount, checkPassword, (req, res) => {
   const { user } = req;
-  res.status(200).json({ message: "Usuário logado com sucesso", user })
+  res.status(200).json(user)
 
 })
 
+
+//inserir dados do form na tabela
+// a função checCad verifica se o usuario já respondeu o form
 app.post('/plot', checCad, (req, res) => {
 
   InsertPlot(req.body);
   res.json({ "statusCode": 200 })
 });
 
+
+//buscar dados da tabela
 app.get('/plot', (req, res) => {
   getData().then(data => {
     res.json(data)
   })
 });
 
+
+//iniciando o servidor
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
