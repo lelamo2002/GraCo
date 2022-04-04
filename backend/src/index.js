dd0ee01ff920312309efe42f69c9a00aec42ba79
leo <211031762@aluno.unb.br>
2022-04-04 16:28:16 -0300

    adição do cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,16 @@
 import { createTablePlot, InsertPlot } from './controller/plot.js'
 import { checkPassword, checksDontExistUserAccount, checksExistsUserAccount, createTableUser, InsertUser } from './controller/user.js'
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.use(express.json())
 const port = 3000
